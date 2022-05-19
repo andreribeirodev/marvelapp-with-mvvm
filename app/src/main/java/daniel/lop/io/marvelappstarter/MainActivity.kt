5ee131f6ad3d5f870f3a2dcd15bc6c6d48f01591
andreribeirodev <andreeleeandro@gmail.com>
2022-05-19 00:09:35 -0300

    adiciona libs e plugins necessarios

--- app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
@@ -1,16 +1,17 @@
 package daniel.lop.io.marvelappstarter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
--- app/src/main/java/daniel/lop/io/marvelappstarter/SplashActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/SplashActivity.kt
@@ -1,18 +1,18 @@
 package daniel.lop.io.marvelappstarter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import daniel.lop.io.marvelappstarter.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
+    private val binding: ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
     }
 }
